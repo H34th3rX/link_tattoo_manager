71ccee82d23264f1b92bbdeb42e8526c8f62d3e3
H34th3rX <heatherpretty1@gmail.com>
2026-01-10 15:10:40 -0400

    #elimiacion de notifiacion_Service duplicado

--- lib/appbar.dart
+++ lib/appbar.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import './l10n/app_localizations.dart';
 import 'theme_provider.dart';
-import '../integrations/notifications_service.dart';
+import '../services/notifications_service.dart';
 import '../integrations/employee_service.dart';
 
 //[-------------CONSTANTES GLOBALES--------------]
